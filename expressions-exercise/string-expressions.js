b409937e7c3e7f0022093a12c9bfd0034384439a
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-18 22:19:59 -0700

    Complete exercises A-D

---
+++ expressions-exercise/string-expressions.js
@@ -0,0 +1,8 @@
+console.log();
+console.log("river" + "town"); // rivertown
+console.log("cat" + "dog");  // catdog
+console.log("New" + " York"); // New York
+console.log("runner's knee" + "!"); // runner's knee!
+console.log("man" + "bear" + "pig"); // manbearpig
+console.log();
+

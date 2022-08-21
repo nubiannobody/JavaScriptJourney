67c4d83def7afec14494be29018b987073a042cb
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-21 10:40:58 -0700

    Complete exercises

---
+++ a-functions-exercise/average.js
@@ -0,0 +1,7 @@
+// Write a function `average` that accepts three numbers as arguments.
+// The function should return the average of three numbers.
+
+console.log(average(3, 10, 8)); // 7
+console.log(average(10, 5, 12)); // 9
+console.log(6, 20, 40); // 22
+
---
+++ a-functions-exercise/endsWithT.js
@@ -0,0 +1,8 @@
+// Write a function `endsWithT` that accepts a string as an argument. 
+// The function should return a boolean 
+// Indicating whether or not the string ends with the character 't'.
+
+console.log(endsWithT("smart")); // true
+console.log(endsWithT("racket")); // true
+console.log(endsWithT("taco")); // false
+console.log(endsWithT("boomerang")) // false

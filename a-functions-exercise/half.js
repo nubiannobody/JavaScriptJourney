b2f036f3480fb1295c7465b3ead0ed6fe78d230e
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-21 10:37:21 -0700

    Complete exercises

---
+++ a-functions-exercise/half.js
@@ -0,0 +1,6 @@
+// Write a function `half` that accepts a number as an argument
+// The function should return half of the number 
+
+console.log(half(8));
+console.log(half(15)); // 
+console.log(half(90)); // 45

fd3cb2d18be499092a4a504bfd9b46428b7bb281
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-21 10:30:09 -0700

    Complete exercises

---
+++ a-functions-exercise/is-div-by-4.js
@@ -0,0 +1,9 @@
+// Write a function `isDivBy4` that accepts a number as an argument. 
+// The function should return a 
+// Boolean indicating whether or not the number is divisible by 4.
+
+console.log(isDivBy4(8));
+console.log(isDivBy4(12));
+console.log(isDivBy4(24));
+console.log(isDivBy4(9));
+console.log(isDivBy4(10));

ec8227aff9fd7aa5bcdbc9ce7a37c30426ad9730
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-21 10:51:35 -0700

    Complete exercises

--- a-functions-exercise/snippet-0-2.js
+++ a-functions-exercise/snippet-0-2.js
@@ -3,13 +3,13 @@ let howMany = function () {
     return 42;
 };
 
-console.log(howMany);
-console.log(howMany());
+console.log(howMany); // undefinded
+console.log(howMany()); // 42
 
 const theAnswer = howMany();
-console.log(theAnswer);
+console.log(theAnswer); // 42
 
 let howMuch = function () {
     5;
 };
-console.log(howMuch());
+console.log(howMuch()); // undefined

b72868e0f304ebe808a3a424945ffdefa6606975
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-21 10:33:12 -0700

    Complete exercises

---
+++ a-functions-exercise/keep-it-quiet.js
@@ -0,0 +1,8 @@
+// Write a function `keepItQuiet` that accepts a string as an argument
+// The function should return the lowercase version of the string with 3 periods added
+// To the end of it
+
+console.log(keepItQuiet("HOORAY")); // 'hooray...'
+console.log(keepItQuiet("Doggo")); // 'doggo...'
+console.log(keepItQuiet("WHAT?!?!")); // 'what?!?!...'
+

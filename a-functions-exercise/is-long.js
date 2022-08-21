de918b1e179d093e709928b0387650d164e0d6bd
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-21 10:35:36 -0700

    Complete exercises

---
+++ a-functions-exercise/is-long.js
@@ -0,0 +1,10 @@
+// Write a function `isLong` that accepts a string as an argument. 
+// The function should return a boolean
+// Indicating whether or not the string is longer than 5 characters
+
+console.log(isLong("pie")); // false
+console.log(isLong("kite"));
+console.log(isLong("kitty"));
+console.log(isLong("telescope"));
+console.log(isLong("thermometer"));
+console.log(isLong("restaurant"));

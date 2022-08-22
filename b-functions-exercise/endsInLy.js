4c5a768d7f454a769f7102797e40fad270e99200
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 09:05:53 -0700

    Complete exercises

---
+++ b-functions-exercise/endsInLy.js
@@ -0,0 +1,9 @@
+// Write a function `endsInLy` that accepts a string as an argument and returns a boolean indicating
+// whether or not the string ends in the substring 'ly'.
+
+console.log(endsInLy("pretty")); // false
+console.log(endsInLy("instant")); // false
+console.log(endsInLy("analytic")); // false
+console.log(endsInLy("timidly")); // true
+console.log(endsInLy("fly")); // true
+console.log(endsInLy("gallantly")); // true

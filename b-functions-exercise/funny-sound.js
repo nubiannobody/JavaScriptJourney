8eac39fafb3ed7a1f7dcc10110f6d130fd0c437f
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 09:05:38 -0700

    Complete exercises

---
+++ b-functions-exercise/funny-sound.js
@@ -0,0 +1,9 @@
+// Write a function `funnySound` that accepts two strings as arguments. The function should return a
+// new string containing the first three characters of both strings concatenated together.
+
+// You can assume that the arguments are both at least three characters long.
+
+console.log(funnySound("tiger", "spoon")); // 'tigspo'
+console.log(funnySound("computer", "phone")); // 'compho'
+console.log(funnySound("skate", "bottle")); // 'skabot'
+console.log(funnySound("frog", "ashtray")); // 'froash'

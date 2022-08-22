302072e02848995500d4ef8ac5b99ed1c1e6d9f2
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 10:05:34 -0700

    Complete exercises

--- b-functions-exercise/string-size.js
+++ b-functions-exercise/string-size.js
@@ -1,6 +1,20 @@
-// Write a function `stringSize` that accepts a string as an argument. The function should return the
-// string 'small' if the argument is shorter than 5 characters, 'medium' if it is exactly 5 characters, and
+// Write a function `stringSize` that accepts a string as an argument. 
+// The function should return the
+// string 'small' if the argument is shorter than 5 characters, 
+// medium' if it is exactly 5 characters, and
 // 'large' if it is longer than 5 characters.
+
+let stringSize = function (str) {
+if (str.length < 5) {
+    return 'small';
+} else if (str.length == 5) {
+    return 'medium';
+} else if (str.length > 5) {
+    return 'large';
+} else {
+    return 'enter a string longer than 3 characters';
+}
+};
 
 console.log(stringSize("cat")); // 'small'
 console.log(stringSize("bell")); // 'small'

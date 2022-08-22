6766edee9f3b7420d51ef4c30507d08dc71781d9
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 09:49:48 -0700

    Complete exercises

--- b-functions-exercise/one-or-none.js
+++ b-functions-exercise/one-or-none.js
@@ -1,5 +1,18 @@
-// Write a function `oneOrNone` that accepts two booleans as arguments. The function should return true
-// if exactly one of the arguments is true. If BOTH arguments are true, then it should return false.
+// Write a function `oneOrNone` that accepts two booleans as arguments. 
+// The function should return true if exactly one of the arguments is true.
+// If BOTH arguments are true, then it should return false.
+
+let oneOrNone = function (bool1, bool2) {
+
+    if (bool1 == true && bool2 == false) {
+        return true;
+    } else if (bool1 == false && bool2 == true) {
+        return true;
+    } else if (bool1 && bool2 == true) {
+        return false;
+    }
+};
+
 
 console.log(oneOrNone(false, false)); // false
 console.log(oneOrNone(true, false)); // true

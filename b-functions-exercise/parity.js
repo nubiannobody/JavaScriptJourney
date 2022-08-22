1382ff5ebc2726e4d6e750c074d159f2638b3767
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 09:37:29 -0700

    Complete exercises

--- b-functions-exercise/parity.js
+++ b-functions-exercise/parity.js
@@ -1,5 +1,14 @@
-// Write a function `parity` that accepts a number as an argument. The function should return the
-// string 'even' if the number is even. It should return the string 'odd' if the number is odd.
+// Write a function `parity` that accepts a number as an argument. 
+// The function should return the string 'even' if the number is even.
+// It should return the string 'odd' if the number is odd.
+
+let parity = function (num) {
+if (num % 2 === 0) {
+    return 'even';
+} else {
+    return 'odd';
+}
+};
 
 console.log(parity(5)); // 'odd'
 console.log(parity(7)); // 'odd'

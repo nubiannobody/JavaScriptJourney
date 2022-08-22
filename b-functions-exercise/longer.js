2672af8e4c47505204036ecc355f3fdd46417a02
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 09:41:15 -0700

    Complete exercises

--- b-functions-exercise/longer.js
+++ b-functions-exercise/longer.js
@@ -1,5 +1,16 @@
-// Write a function `longer` that accepts two strings as arguments. The function should return the
-// string that is longer. If the strings have the same length, then return the first string.
+// Write a function `longer` that accepts two strings as arguments.
+// The function should return the string that is longer. 
+// If the strings have the same length, then return the first string.
+
+let longer = function (str1, str2) {
+    if (str1.length > str2.length) {
+        return str1;
+    } else if (str1.length == str2.length) {
+        return str1;
+    } else {
+        return str2;
+    }
+}
 
 console.log(longer("drum", "piranha")); // 'piranha'
 console.log(longer("basket", "fork")); // 'basket'

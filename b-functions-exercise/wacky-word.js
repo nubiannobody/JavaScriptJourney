e7757bef6cb49c23d88f8fb1d62ae40c5c863694
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 12:01:43 -0700

    Complete exercises

--- b-functions-exercise/wacky-word.js
+++ b-functions-exercise/wacky-word.js
@@ -12,6 +12,11 @@ let newString = substr1 + substr2;
 return newString;
 };
 
+// Alvin's way 
+let wackyWord = function (str1, str2) {
+    return str1.slice(0, 3) + str2.slice(-2);
+};
+
 console.log(wackyWord("very", "kindly")); // 'verly'
 console.log(wackyWord("forever", "sick")); // 'forck'
 console.log(wackyWord("cellar", "door")); // 'celor'

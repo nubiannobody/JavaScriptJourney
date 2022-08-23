96c4ca50b03340e4100ed505fe2a54bb3a63f988
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 21:02:30 -0700

    Complete exercises

--- c-functions-exercise/average-of-four.js
+++ c-functions-exercise/average-of-four.js
@@ -1,6 +1,12 @@
 // Write a function `averageOfFour(num1, num2, num3, num4)` that accepts four numbers as arguments. 
 // The function should return the average of all four numbers.
 
+let averageOfFour = function (n1, n2, n3, n4) {
+    let sum = n1 + n2 + n3 + n4;
+    let average = sum / 4;
+    return average;
+}
+
 console.log(averageOfFour(10, 4, 12, 3)); // 7.25
 console.log(averageOfFour(-20, 50, 4, 21)); // 13.75
 console.log(averageOfFour(10, 4, 12, 3)); // 7.25

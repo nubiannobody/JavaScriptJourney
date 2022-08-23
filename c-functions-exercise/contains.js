05d7ddcdc1e8564111861b2541ec09a3838422c9
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 21:22:24 -0700

    Complete exercises

--- c-functions-exercise/contains.js
+++ c-functions-exercise/contains.js
@@ -2,6 +2,12 @@
 // return a boolean indicating whether or not `str2` is contained within `str1`. The function should
 // ignore any differences in capitalization.
 
+let contains = function (str1, str2) {
+if (str2.indexOf(str1)) {
+    return true;
+}
+};
+
 console.log(contains("caterpillar", "pill")); // true
 console.log(contains("lion's share", "on")); // true
 console.log(contains("SORRY", "or")); // true
--- c-functions-exercise/larger.js
+++ c-functions-exercise/larger.js
@@ -1,6 +1,14 @@
 // Write a function `larger` that accepts two numbers as arguments. 
 // The function should return the larger number.
 
+let larger = function (num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
 console.log(larger(256, 400)); // 400
 console.log(larger(31, 4)); // 31
 console.log(larger(-6, 7)); // 7

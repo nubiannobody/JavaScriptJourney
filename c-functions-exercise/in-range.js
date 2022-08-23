e9d951860d925ae126a41e6438c8f26317b888db
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-22 21:00:30 -0700

    Complete exercises

--- c-functions-exercise/in-range.js
+++ c-functions-exercise/in-range.js
@@ -1,6 +1,14 @@
 // Write a function `inRange(min, max, n)` that accepts three numbers as arguments. 
 // The function should return a boolean indicating if `n` is between `min` and `max` inclusive.
 
+let inRange = function (min, max, n) {
+    if (n >= min && n <= max) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
 console.log(inRange(5, 13, 8)); // true
 console.log(inRange(5, 13, 29)); // false
 console.log(inRange(100, 125, 100)); // true

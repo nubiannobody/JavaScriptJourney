aabcd4f7bc815cc6a9ed9a9f913ca8cc75b85e63
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-24 15:39:07 -0700

    Complete exercises

---
+++ c-loops/reverse-iterate.js
@@ -0,0 +1,18 @@
+// Write a function `reverseIterate` that accepts a string as an argument. The function should print
+// the characters of the string one by one, in reverse order. The function doesn't need to return any
+// value. It should just print to the terminal.
+
+reverseIterate("carrot");
+// prints
+//  t
+//  o
+//  r
+//  r
+//  a
+//  c
+
+reverseIterate("box");
+// prints
+//  x
+//  o
+//  b

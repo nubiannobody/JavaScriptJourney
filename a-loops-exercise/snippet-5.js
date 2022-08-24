ea6e278cc1bb66ed33b87f4044d0894fba2afc27
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-23 18:32:18 -0700

    Complete exercises

--- a-loops-exercise/snippet-5.js
+++ a-loops-exercise/snippet-5.js
@@ -6,3 +6,21 @@ for (let i = 1; i < 5; i++) {
 }
 
 console.log("grand total: " + total);
+
+// total = 0
+// i = 1
+// total = 1
+// i = 2
+// total 3
+// i = 3
+// total = 6
+// i = 4
+// total = 10
+// i = 5;
+
+// this code will print 
+// 1
+// 3
+// 6
+// 10
+// grand total = 10

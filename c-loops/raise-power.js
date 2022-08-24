91a8bf029772f948831d7580afad787b75ca3e7c
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-24 16:35:28 -0700

    Complete exercises

--- c-loops/raise-power.js
+++ c-loops/raise-power.js
@@ -4,6 +4,10 @@
 // For example, raisePower(2, 5) should return 32 because 2 * 2 * 2 * 2 * 2 = 32 
 // For example, raisePower(4, 3) should return 64 because 4 * 4 * 4 = 64
 
+let raisePower = function (base, exponent) {
+    return base ** exponent;
+}
+
 console.log(raisePower(2, 5)); // 32
 console.log(raisePower(4, 3)); // 64
 console.log(raisePower(10, 4)); // 10000

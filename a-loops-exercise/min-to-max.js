b3583c6a2d0f71bd1ae27ddf2b7c7a529ca072cc
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-23 23:43:08 -0700

    Complete exercises

--- a-loops-exercise/min-to-max.js
+++ a-loops-exercise/min-to-max.js
@@ -2,6 +2,12 @@
 // print all numbers from min to max inclusive. The function doesn't need to return any value. It
 // should just print to the terminal.
 
+let minToMax = function (min, max) {
+    for (let i = min; i <= max; i++) {
+        console.log(i);
+    }
+}
+
 minToMax(5, 9);
 // prints
 //  5
@@ -9,6 +15,8 @@ minToMax(5, 9);
 //  7
 //  8
 //  9
+
+console.log();
 
 minToMax(11, 13);
 // prints

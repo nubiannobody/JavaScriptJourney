6a90c3ccc59601310592a66f23da5d1ef39433ce
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-24 16:32:10 -0700

    Complete exercises

--- c-loops/remove-capitals.js
+++ c-loops/remove-capitals.js
@@ -1,6 +1,18 @@
 // Write a function `removeCapitals` that accepts a string as an argument. The function should return a
 // new version of the string with all capital letters removed.
 
+let removeCapitals = function (str) {
+    let noCapitals = '';
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === char.toLowerCase()) {
+           noCapitals += char; 
+        }
+    }
+
+    return noCapitals;
+};
+
 console.log(removeCapitals("fOrEver")); // 'frver'
-console.log(removeCapitals("raiNCoat")); // 'raioat'
-console.log(removeCapitals("cElLAr Door")); // 'clr oor'
+// console.log(removeCapitals("raiNCoat")); // 'raioat'
+// console.log(removeCapitals("cElLAr Door")); // 'clr oor'

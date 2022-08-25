50823ee30c4167a3110468baef1fdcc3c2029267
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-24 19:35:13 -0700

    Complete exercises

--- b-loops/odd-sum.js
+++ b-loops/odd-sum.js
@@ -3,5 +3,18 @@
 //
 // For example, oddSum(10) should return 25 because 1 + 3 + 5 + 7 + 9 = 25
 
+let oddSum = function (max) {
+let total = 0;
+for (let i = 1; i <= max; i++){
+    if (i % 2 !==0) {
+       total += i;
+    }
+}
+return total;
+};
+
 console.log(oddSum(10)); // 25
+
+console.log();
+
 console.log(oddSum(5)); // 9

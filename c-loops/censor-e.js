afe71ed8765908eaaaa835adf88662721277931f
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-24 17:12:23 -0700

    Complete exercises

--- c-loops/censor-e.js
+++ c-loops/censor-e.js
@@ -2,16 +2,16 @@
 // version of string where all characters that are 'e's are replaced with '*'s.
 
 let censorE = function (str) {
-    let noE = '';
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        if (char === 'e'){
-            noE += '*';
-        } else {
-            noE += char;
+    let noE = ''; // empty string to store *
+    for (let i = 0; i < str.length; i++) { // loop to iterate through entire string
+        let char = str[i]; // single characters of string
+        if (char === 'e'){ // checks if the characters equal e
+            noE += '*'; // adds * to empty string
+        } else { 
+            noE += char; // adds characters that are not e to the empty string
         }
     }
-    return noE;
+    return noE; // returns new string with e replaced with *
 }
 
 

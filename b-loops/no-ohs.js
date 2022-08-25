134e45d7294937bb5c6ebbd5721479f6a44a82f5
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-24 18:16:53 -0700

    Complete exercises

--- b-loops/no-ohs.js
+++ b-loops/no-ohs.js
@@ -2,11 +2,25 @@
 // characters of the string one by one except the character 'o'. The function doesn't need to return
 // any value. It should just print to the terminal.
 
+let noOhs = function (str) {
+    let o = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'o') {
+         str[i] += o;
+        } else {
+          console.log(str[i]);
+        }
+       
+    }
+}
+
 noOhs("code");
 // prints
 //  c
 //  d
 //  e
+
+console.log();
 
 noOhs("school");
 // prints

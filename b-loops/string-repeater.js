88591257310c760d8558e04606afacaa944416c7
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-08-26 07:17:38 -0700

    Complete exercises

--- b-loops/string-repeater.js
+++ b-loops/string-repeater.js
@@ -2,12 +2,10 @@
 // The function should return a new string consisting of the `str` repeated `num` number of times.
 
 let stringRepeater = function (str, num) {
-    let newString = '';
-for (let i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
+        console.log(i * num);
+    }
 }
-return str += str;
-};
-
 console.log(stringRepeater("q", 4)); // 'qqqq'
 console.log(stringRepeater("go", 2)); // 'gogo'
 console.log(stringRepeater("tac", 3)); // 'tactactac'
